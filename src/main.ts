b2a25c843a121df136ff84943bffecd9587407a1
mjagetic-margins <marko.jagetic@valeremargins.com>
2024-10-11 15:22:38 +0200

    Fix cors origins

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
   app.enableCors({
+    origin: process.env.CLIENT_URL,
     credentials: true,
   });
   await app.listen(PORT);
